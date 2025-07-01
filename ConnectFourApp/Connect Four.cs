a6e33edcf9a3de6935d234d48e8fd3ccefdc68f1
pessyfeder <pessyf19@gmail.com>
2025-07-01 16:05:10 -0400

    updated table layout panels and buttons properties in designer

--- ConnectFourApp/Connect Four.cs
+++ ConnectFourApp/Connect Four.cs
@@ -4,16 +4,19 @@ namespace ConnectFourApp
     {
         Random rnd = new();
 
-        List<Button> lstBtnRow1;
-        List<Button> lstBtnRow2;
-        List<Button> lstBtnRow3;
-        List<Button> lstBtnRow4;
-        List<Button> lstBtnRow5;
-        List<Button> lstBtnRow6;
+        List<Button> lstBtnColumn1;
+        List<Button> lstBtnColumn2;
+        List<Button> lstBtnColumn3;
+        List<Button> lstBtnColumn4;
+        List<Button> lstBtnColumn5;
+        List<Button> lstBtnColumn6;
 
-        List<List<Button>> lstBtnRowLists;
+        List<List<Button>> lstBtnColumnLists;
 
-        List<List<Button>> lstWinningSets;
+        List<List<Button>> lstVerticalWinningSets;
+        List<List<Button>> lstHorizontalWinningSets;
+        List<List<Button>> lstDiagonalLeftRightWinningSets;
+        List<List<Button>> lstDiagonalRightLeftWinningSets;
 
         bool playAgainstComp = false;
         private enum TurnEnum { red, white };
@@ -26,16 +29,16 @@ namespace ConnectFourApp
         {
             InitializeComponent();
 
-            lstBtnRow1 = new() { button1, button2, button3, button4, button5, button6 };
-            lstBtnRow2 = new() { button7, button8, button9, button10, button11, button12 };
-            lstBtnRow3 = new() { button13, button14, button15, button16, button17, button18 };
-            lstBtnRow4 = new() { button19, button20, button21, button22, button23, button24 };
-            lstBtnRow5 = new() { button25, button26, button27, button28, button29, button30 };
-            lstBtnRow6 = new() { button31, button32, button33, button34, button35, button36 };
+            lstBtnColumn1 = new() { button1, button2, button3, button4, button5, button6 };
+            lstBtnColumn2 = new() { button7, button8, button9, button10, button11, button12 };
+            lstBtnColumn3 = new() { button13, button14, button15, button16, button17, button18 };
+            lstBtnColumn4 = new() { button19, button20, button21, button22, button23, button24 };
+            lstBtnColumn5 = new() { button25, button26, button27, button28, button29, button30 };
+            lstBtnColumn6 = new() { button31, button32, button33, button34, button35, button36 };
 
-            lstBtnRowLists = new() { lstBtnRow1, lstBtnRow2, lstBtnRow3, lstBtnRow4, lstBtnRow5, lstBtnRow6 };
+            lstBtnColumnLists = new() { lstBtnColumn1, lstBtnColumn2, lstBtnColumn3, lstBtnColumn4, lstBtnColumn5, lstBtnColumn6 };
 
-            lstWinningSets = new()
+            lstVerticalWinningSets = new()
             {
                 new() { button1, button2, button3, button4 },
                 new() { button2, button3, button4, button5 },
@@ -46,7 +49,74 @@ namespace ConnectFourApp
                 new() { button13, button14, button15, button16 },
                 new() { button17, button14, button15, button16 },
                 new() { button17, button18, button15, button16 },
+                new() { button19, button20, button21, button22 },
+                new() { button20, button21, button22, button23 },
+                new() { button24, button21, button22, button23 },
+                new() { button25, button26, button27, button28 },
+                new() { button29, button26, button27, button28 },
+                new() { button29, button30, button27, button28 },
+                new() { button33, button34, button31, button32 },
+                new() { button33, button34, button35, button32 },
+                new() { button33, button34, button35, button36 },
+                new() { button37, button38, button39, button40 },
+                new() { button41, button38, button39, button40 },
+                new() { button41, button42, button39, button40 },
+            };
 
+            lstHorizontalWinningSets = new()
+            {
+                new() { button1, button7, button13, button19 },
+                new() { button25, button7, button13, button19 },
+                new() { button25, button31, button13, button19 },
+                new() { button25, button31, button37, button19 },
+                new() { button2, button8, button14, button20 },
+                new() { button26, button8, button14, button20 },
+                new() { button14, button20, button26, button32 },
+                new() { button20, button26, button32, button38 },
+                new() { button3, button9, button15, button21 },
+                new() { button9, button15, button21, button27 },
+                new() { button15, button21, button27, button33 },
+                new() { button21, button27, button33, button39 },
+                new() { button4, button10, button16, button22 },
+                new() { button10, button16, button22, button28 },
+                new() { button16, button22, button28, button34 },
+                new() { button22, button28, button34, button40 },
+                new() { button5, button11, button17, button23 },
+                new() { button11, button17, button23, button29 },
+                new() { button17, button23, button29, button35 },
+                new() { button23, button29, button35, button41 },
+                new() { button6, button12, button18, button24 },
+                new() { button12, button18, button24, button30 },
+                new() { button18, button24, button30, button36 },
+                new() { button24, button30, button36, button42 },
+            };
+
+            lstDiagonalLeftRightWinningSets = new()
+            {
+                new() { button3, button10, button17, button24 },
+                new() { button2, button9, button16, button23 },
+                new() { button9, button16, button23, button30 }, 
+                new() { button1, button8, button15, button22 }, 
+                new() { button8, button15, button22, button29 },
+                new() { button15, button22, button29, button36 },
+                new() { button7, button14, button21, button28 },
+                new() { button14, button21, button28, button35 },
+                new() { button21, button28, button35, button42 },
+                new() { button13, button20, button27, button34 },
+                new() { button20, button27, button34, button41 },
+                new() { button19, button26, button33, button40 }
+            };
+
+            lstDiagonalRightLeftWinningSets = new()
+            {
+                new() { button39, button34, button29, button24 },
+                new() { button18, button23, button28, button33 },
+                new() { button23, button28, button33, button38 },
+                new() { button12, button17, button22, button27 },
+                new() { button17, button22, button27, button32 },
+                new() { button22, button27, button32, button37 },
+                new() { button6, button11, button16, button21 },
+                new() { button11, button16, button21, button26 },
             };
 
 
